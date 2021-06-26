4d08004bb93248a0861a3e91fa89fb59bb28df48
ElyorbekMamaras <>
2021-06-26 06:27:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int u;
+
+  for (int q=1; q<=10; q++) {
+    u = q;
+    while (u<=10){
+      cout << u << " ";
+      u++;
+    }
+    int k = 1;
+    while(k<q){
+      cout << k << " ";
+      k++;
+    }
+
+    cout << endl;
+  }
+  
+
+}
